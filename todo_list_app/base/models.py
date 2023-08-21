5241540e44e7f6aac6cb7f7c54aab174e57b5cf5
Adnan Sabir <adnansabir147@gmail.com>
2023-08-21 11:35:42 +0500

    delete date model

--- todo_list_app/base/models.py
+++ todo_list_app/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 # Create your models here.
 
@@ -9,11 +8,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
     
-    # @property
-    # def convert_to_local_time(self):
-    #     return timezone.localtime(self.created)
 
     def __str__(self):
         return self.title
