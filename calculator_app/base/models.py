322018495f47236f16d8ffd7f313a495dd8ab70c
Adnan Sabir <adnansabir147@gmail.com>
2023-09-01 06:47:32 +0500

    changes in css

--- calculator_app/base/models.py
+++ calculator_app/base/models.py
@@ -5,6 +5,3 @@ class Calculation(models.Model):
     num2 = models.FloatField()
     operation = models.CharField(max_length=10)
     result = models.FloatField()
-
-    # def __str__(self):
-    #     return f"{self.num1} {self.operation} {self.num2} = {self.result}"
--- calculator_app/base/views.py
+++ calculator_app/base/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-from .models import Calculation
-
 from .models import Calculation
 
 def index(request):
     if request.method == 'POST':
-        num1 = float(request.POST['num1'])
-        num2 = float(request.POST['num2'])
+        num1 = request.POST['num1']
+        num2 = request.POST['num2']
         operation = request.POST['operation']
+
+        try:
+            num1 = float(num1)
+            num2 = float(num2)
+        except ValueError:
+            error_message = "Please enter valid numbers"
+            return render(request, 'base/index.html', {'error_message': error_message})
 
         if operation == 'add':
             result = num1 + num2
@@ -17,11 +21,15 @@ def index(request):
         elif operation == 'multiply':
             result = num1 * num2
         elif operation == 'divide':
+            if num2 == 0:
+                error_message = "Cannot divide by zero"
+                return render(request, 'base/index.html', {'error_message': error_message})
             result = num1 / num2
 
         calculation = Calculation(num1=num1, num2=num2, operation=operation, result=result)
         calculation.save()
 
-        return render(request, 'base/index.html', {'result': result, 'num1': num1, 'num2' : num2, 'operation' : operation})
+        return render(request, 'base/index.html', {'result': result, 'num1': num1, 'num2': num2, 'operation': operation})
 
     return render(request, 'base/index.html')
+
